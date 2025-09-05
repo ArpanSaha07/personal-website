c13a7a752a61772ccfd205bba26e0562faabe760
Arpan Saha <sahaarpan550@gmail.com>
2025-09-04 23:46:27 -0400

    Updated text in AboutMe section, also added text-justify

--- app/components/AboutMeSection.tsx
+++ app/components/AboutMeSection.tsx
@@ -2,12 +2,13 @@ export default function AboutMe() {
     return (
         <div className="bg-gray-100 relative py-25 px-4 md:px-20 flex flex-col items-center">
             
-            <div className="m-2 text-lg text-gray-600 max-w-2xl">
+            <div className="m-2 text-lg text-gray-600 max-w-2xl text-justify">
                 <h1 className="section-heading pb-8">About Me</h1>
-                <p>I am a software developer studying at McGill University. I play football (⚽), volleyball(🏐) and badminton(🏸). I am a F1 fanatic (🏎️), drinking some hopium to see a Williams win soon😂.</p>
+                <p>I am a software developer, with experience in full-stack development and machine learning. I graduated from McGill University with a Bachelor&apos;s in Computer Science and Statistics.</p>
+                <p>I play football (⚽), volleyball(🏐) and badminton(🏸). I am a F1 fanatic (🏎️), drinking some hopium to see a Williams win soon😂.</p>
                 <p>I love nature. You&apos;ll usually find me biking by the Saint Laurent river or hiking up Mont Royal.</p>
-
-                <p>I can speak 5 languages. Currently I am working at my company ElevateTech, where I provide tech solutions to businesses and individuals.</p>
+                <p>I can speak 5 languages. I am currently learning to play guitar.</p>
+                <p>I provide tech solutions to businesses and individuals through my company ElevateTech. I am looking for work and collaboration opportunities, so feel free to reach out!</p>
             </div>
         </div>
     )
